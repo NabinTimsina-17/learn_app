ba56b7dbedd7bf6d776745f8621ce219600c5c64
NabinTimsina-17 <133372145+NabinTimsina-17@users.noreply.github.com>
2024-10-25 17:02:33 +0545

    .

--- lib/setup.dart
+++ lib/setup.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:learn_app/signin.dart';
 
 class SetUp extends StatefulWidget {
   const SetUp({super.key});
@@ -10,6 +11,63 @@ class SetUp extends StatefulWidget {
 class _SetUpState extends State<SetUp> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black87,
+        elevation: 0,
+        leading: IconButton(
+          icon: const Icon(
+            Icons.arrow_back,
+            color: Colors.white,
+          ),
+          onPressed: () {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => const SignIn()));
+          },
+        ),
+        title: Image.asset('assets/images/logo.png'),
+        actions: [
+          TextButton(
+            onPressed: () {},
+            child: const Text(
+              'Skip',
+              style: TextStyle(
+                color: Colors.grey,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+          ),
+        ],
+        bottom: PreferredSize(
+            preferredSize: const Size.fromHeight(4.0),
+            child: Container(
+              height: 4.0,
+              width: double.infinity,
+              color: Colors.grey[300],
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Container(
+                  width: MediaQuery.of(context).size.width * 0.5,
+                  color: Colors.purpleAccent,
+                ),
+              ),
+            )),
+      ),
+      body: const Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 25,
+            vertical: 12),
+            child: Text("Fill in your User Details",
+                style: TextStyle(
+                  fontSize: 25,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+                ),
+          ),
+        ],
+      ),
+    );
   }
 }
